56c843e69affdf7e00ab669385be57799f2a628d
Alberto de Lucas <alberto5dlg@gmail.com>
2018-04-25 09:33:19 +0200

    Script inicio servicios

---
+++ Raspberry/scriptInicio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Iniciando proyectos ...
+
+node Desktop/prjectTFG/App/server/index.js
+
+echo Servidor Node Iniciado
+
+ng serve DesktopÇ/projectTFG/App/client
+
+echo Cliente Angular Iniciado
